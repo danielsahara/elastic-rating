37bbb75db22278a7f6875b30a29cd2c450da1bea
dsahara.ext@vr.com.br <brenda11>
2018-06-11 22:47:12 -0300

    swagger config

--- elastic-rating-services/src/main/java/com/elasticrating/controller/RatingController.java
+++ elastic-rating-services/src/main/java/com/elasticrating/controller/RatingController.java
@@ -8,14 +8,19 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
 public class RatingController {
     @Autowired
     private RatingService ratingService;
 
+
+    @ApiOperation(value = "Search rating.",
+            notes = "Search rating.",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
     @GetMapping(value = "/ratings")
-    @ApiOperation(value = "Find rating",
-            notes = "Find rating via parameter")
     public List<Rating> list(@RequestParam(value = "idControl", required = false) String idControl,
                              @RequestParam(value = "rating", required = false) String rating,
                              @RequestParam(value = "type", required = false) String type,
@@ -26,11 +31,19 @@ public class RatingController {
         return ratingService.list(idControl, rating, type, description, from, size);
     }
 
+    @ApiOperation(value = "Save rating",
+            notes = "Save rating",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
     @PostMapping(value = "/ratings")
     public void create(@RequestBody Rating rating) {
         ratingService.save(rating);
     }
 
+    @ApiOperation(value = "Update a rating",
+            notes = "Update a rating",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
     @PatchMapping(value = "/ratings")
     public void update(@RequestBody Rating rating) {
         ratingService.update(rating);
--- elastic-rating-services/src/main/java/com/elasticrating/model/Rating.java
+++ elastic-rating-services/src/main/java/com/elasticrating/model/Rating.java
@@ -7,16 +7,25 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@ApiModel(description = "Model of rating")
+@ApiModel(description = "Model of rating.")
 public class Rating {
     private String id;
 
-    @ApiModelProperty(notes="Id of control")
+    @ApiModelProperty(notes="Id of the thing rated.")
     private String idControl;
+
+    @ApiModelProperty(notes="Name of thing rated.")
     private String nameControl;
-    @ApiModelProperty(notes="Rating")
+
+    @ApiModelProperty(notes="Rating.")
     private int rating;
+
+    @ApiModelProperty(notes="Type of rating.")
     private String type;
+
+    @ApiModelProperty(notes="Description of rating.")
     private String description;
+
+    @ApiModelProperty(notes="Status of rating.")
     private boolean status;
 }
